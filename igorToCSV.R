74853736643a649f2c95196ba50eb2f31a2b2a2f
yuwie10 <yuwie10@gmail.com>
2016-11-14 20:53:08 -0500

    filtering waves in python

--- igorToCSV.R
+++ igorToCSV.R
@@ -1,22 +1,17 @@
 library(IgorR)
 library(purrr)
+library(magrittr)
 
 ##Convert raw Igor file to csv for python
 
-#convert igor file to ts
-rawData <- read.pxp("2016.10.26_YW", regex = "w.", ReturnTimeSeries = TRUE)
-rawWaves <- rawData[-1]
-
-#annotate data
+#Fxn to annotate data
 toSplitData <- function(ts) {
   df <- data.frame(pA = c(ts), sec = c(time(ts)), id = attr(ts, "WaveHeader")$WaveName)
   df$id <- as.character(df$id)
   return(df)
 }
 
-splitWaves <- purrr::map(rawWaves, toSplitData)
-
-#stack list
+#Fxn to stack list
 listToDataFrame <- function(tracesList) {
   df <- data.frame(
     id = unlist(lapply(tracesList, "[[", "id")),
@@ -27,6 +22,14 @@ listToDataFrame <- function(tracesList) {
   return(df)
 }
 
-dfWaves <- listToDataFrame(splitWaves)
+
+#convert igor file to list of ts
+rawData <- read.pxp("2016.10.26_YW", regex = "w.", ReturnTimeSeries = TRUE)
+#remove vars sublist
+rawData <- rawData[-1]
+
+#create data frame with sec, pA, id
+dfWaves <- purrr::map(rawData, toSplitData) %>%
+  listToDataFrame()
 
 write.csv(dfWaves, file = "igorForPython.csv")
